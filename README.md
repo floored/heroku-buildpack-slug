5d69d979538b3aa4d7d4671a36c69c7f6c408643
Daniel Suo <danielsuo@gmail.com>
2015-05-18 15:26:02 -0400

    pushing slugify responsibilities to project

--- README.md
+++ README.md
@@ -7,12 +7,19 @@ Create slug from build directory and environment
 - Upload to S3 bucket
 
 ## Testing
+- In the build directory
+  - Add a file called 'Slugify' and implements three functions:
+    - slugify: turns build directory into slug
+    - upload: uploads slug to location
+    - cleanup: cleans up
+  - Add this buildpack repository to .buildpacks
 - Create Heroku application
-- Set config variables
-  - ```AWS_ACCESS_KEY```
-  - ```AWS_SECRET_KEY```
-  - ```AWS_SLUG_BUCKET```
-  - ```NODE_ENV=production```
-- Add heroku-buildpack-multi
-  - ```heroku config:add BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git```
+  - Set config variables
+    - ```AWS_ACCESS_KEY```
+    - ```AWS_SECRET_KEY```
+    - ```AWS_SLUG_BUCKET```
+    - ```NODE_ENV=production```
+  - Add heroku-buildpack-multi
+    - ```heroku config:add BUILDPACK_URL=https://github.com/ddollar/heroku-buildpack-multi.git```
+- Push to heroku master
 - Verify that correct tar is uploaded to S3
--- bin/utils.sh
+++ bin/utils.sh
@@ -1,9 +1,3 @@
-# Function definitions
-error() {
-  echo " !     $*" >&2
-  exit 1
-}
-
 status() {
   echo "-----> $*"
 }
@@ -17,33 +11,4 @@ indent() {
     Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
     *)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
   esac
-}
-
-export_env_dir() {
-  env_dir=$1
-  whitelist_regex=${2:-''}
-  blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
-
-  if [ -d "$env_dir" ]; then
-    # Write opening brace
-    echo { >> env.json
-
-    # Loop through each environment variable
-    for e in $(ls $env_dir); do
-      # echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
-      # export "$e=$(cat $env_dir/$e)"
-      # :
-      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
-      echo \"$e\": \"$(cat $env_dir/$e)\", >> env.json
-    done
-
-    # Remove final comma
-    case $(uname) in
-      Darwin) sed -i '' '$s/.$//' env.json;; # mac/bsd sed: -i requires extension
-      *)      sed -i '$s/.$//' env.json;; # unix/gnu sed: -i does not require extension
-    esac
-
-    # Write closing brace
-    echo } >> env.json
-  fi
 }
